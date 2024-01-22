56e3ce12b83c09a3f0156ea0dac048c296727e47
Eunryong <86572427+Eunryong@users.noreply.github.com>
2024-01-22 19:17:57 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <list>
+#include <utility>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int N;
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cout << i << "\n";
+    }
+
+    return 0;
+}
