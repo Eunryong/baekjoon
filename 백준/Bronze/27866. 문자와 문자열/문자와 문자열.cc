663ed0974f57452a98609f8a1e052ed878407ce3
Eunryong <86572427+Eunryong@users.noreply.github.com>
2024-01-22 19:14:51 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <list>
+#include <utility>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    string S;
+    int i;
+
+    cin >> S;
+    cin >> i;
+    cout << S[i -1];
+
+    return 0;
+}
