4f60efc037a2627feee2768eded66e9c1cc4159d
Eunryong <86572427+Eunryong@users.noreply.github.com>
2024-02-13 02:13:36 +0900

    [Platinum V] Title: 히스토그램, Time: 12 ms, Memory: 2940 KB -BaekjoonHub

---
+++ 백준/Platinum/1725. 히스토그램/히스토그램.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <list>
+#include <utility>
+#include <cmath>
+using namespace std;
+int N, ans, h, w;
+int arr[100002];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> N;
+    stack<int> st;
+    for (int i = 1; i <= N; i++) cin >> arr[i];
+    st.push(0);
+    for (int i = 1; i <= N + 1; i++) {
+        while (arr[i] < arr[st.top()] && !st.empty()) {
+            h = arr[st.top()];
+            st.pop();
+            w = i - st.top() - 1;
+            ans = max(ans, h * w);
+        }
+        st.push(i);
+    }
+    cout << ans;
+    return 0;
+}
