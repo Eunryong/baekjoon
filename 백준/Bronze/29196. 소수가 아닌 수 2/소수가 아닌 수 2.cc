1fb866d26535bcb2a2ae3519d79f73a131587b53
Eunryong <86572427+Eunryong@users.noreply.github.com>
2024-02-23 20:02:37 +0900

    [Bronze I] Title: 소수가 아닌 수 2, Time: 0 ms, Memory: 2020 KB, Score: 15 point -BaekjoonHub

---
+++ 백준/Bronze/29196. 소수가 아닌 수 2/소수가 아닌 수 2.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <list>
+#include <utility>
+#include <cmath>
+using namespace std;
+double d;
+
+int gcd(int a, int b) {
+    int r;
+    while(b != 0) {
+        r = a % b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> d;
+    int n = d * 1000000;
+    if (n != d * 1000000) {
+        cout << "NO\n";
+        return 0;
+    }
+    int a = gcd(n, 1000000);
+    cout << "YES\n";
+    cout << n / a << " " << 1000000 / a;
+    
+    return 0;
+}
