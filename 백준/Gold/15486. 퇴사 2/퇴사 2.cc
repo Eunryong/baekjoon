eb7f252cfab90aaf8c88a89466b43670ca0daf90
Eunryong <86572427+Eunryong@users.noreply.github.com>
2024-04-17 15:45:19 +0900

    [Gold V] Title: 퇴사 2, Time: 536 ms, Memory: 312568 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <list>
+#include <utility>
+#include <cmath>
+using namespace std;
+int N;
+int p[1500001], t[1500001], dp[1500001][51];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cin >> t[i] >> p[i];
+    }
+    for (int i = 1; i <= N; i++) {
+        dp[i][0] = dp[i - 1][0];
+        for (int j = 0; j <= 50; j++) {
+            if (j == 0) {
+                dp[i][t[i] - 1] = max(dp[i][t[i] - 1], dp[i][j] + p[i]);
+            } else {
+                dp[i][j - 1] = max(dp[i][j - 1], dp[i - 1][j]);
+            }
+        }
+    }
+    cout << dp[N][0];
+    return 0;
+}
