1bb4d84ea531bfba94edf470ee40aa245ce4230e
Eunryong <86572427+Eunryong@users.noreply.github.com>
2024-04-29 11:12:41 +0900

    [Gold III] Title: 당근 클릭 게임, Time: 36 ms, Memory: 4080 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31434. 당근 클릭 게임/당근 클릭 게임.cc
+++ 백준/Gold/31434. 당근 클릭 게임/당근 클릭 게임.cc
@@ -11,40 +11,33 @@
 #include <cmath>
 using namespace std;
 int n, k;
-pair<int, int> arr[101];
-int dp[101][5002];
-bool check[5002];
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     cin >> n >> k;
-    for (int i = 1; i <= n; i++) {
+    int max_speed = 0;
+    vector<pair<int, int> > arr(n, make_pair(0, 0));
+    for (int i = 0; i < n; i++) {
         cin >> arr[i].first >> arr[i].second;
+        max_speed = max(max_speed, arr[i].second);
     }
-    check[1] = 1;
+    max_speed = max_speed * k + 1;
+    vector<vector<int> > dp(k + 1, vector<int>(max_speed + 1, -1));
+    dp[0][1] = 0;
+    int ans = 0;
     for (int i = 1; i <= k; i++) {
-        queue<int> q;
-        for (int j = 1; j <= 5001; j++) {
-            if (check[j] == false) {
+        for (int j = 0; j <= max_speed; j++) {
+            if (dp[i - 1][j] == -1) {
                 continue;
             }
-            dp[i][j] = dp[i - 1][j] + j;
-            for (int k = 1; k <= n; k++) {
-                if (dp[i - 1][j] < arr[k].first) {
-                    continue;
-                } else {
-                    dp[i][j + arr[k].second] = dp[i - 1][j] - arr[k].first;
-                    q.push(j + arr[k].second);
-                }
+            dp[i][j] = max(dp[i][j], dp[i - 1][j] + j);
+            ans = max(dp[i][j], ans);
+            for (int l = 0; l < n; l++) {
+                dp[i][j + arr[l].second] = max(dp[i][j + arr[l].second], dp[i - 1][j] - arr[l].first);
             }
         }
-        while (!q.empty()) {
-            int x = q.front();
-            q.pop();
-            check[x] = 1;
-        }
     }
-    cout << *max_element(dp[k], dp[k] + 5002);
+    cout << ans;
     return 0;
 }
