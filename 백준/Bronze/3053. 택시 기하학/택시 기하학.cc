a1b182f86e10c19effc487a7c3861e8fc2a2d603
Eunryong <86572427+Eunryong@users.noreply.github.com>
2024-05-15 17:31:26 +0900

    [Bronze III] Title: 택시 기하학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <list>
+#include <utility>
+#include <cmath>
+using namespace std;
+int n;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> n;
+    cout.precision(4);
+    cout << fixed;
+    cout << n * n * M_PI << "\n";
+    cout << n * n * 2;
+    return 0;
+}
