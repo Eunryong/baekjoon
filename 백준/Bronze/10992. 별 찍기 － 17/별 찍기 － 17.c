d8a5ce1f03dbaf0ca13fa3268fc96488c34cc3f8
Eunryong <86572427+Eunryong@users.noreply.github.com>
2024-05-23 20:30:43 +0900

    [Bronze III] Title: 별 찍기 - 17, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        if (i == 0) {
+            for (int j = 0; j < n - 1; j++) {
+                printf(" ");
+            }
+            printf("*");
+        } else if (i == n - 1) {
+            for (int j = 0; j < (n - 1) * 2 + 1; j++) {
+                printf("*");
+            }
+        } else {
+            for (int j = 0; j < n - i - 1; j++) {
+                printf(" ");
+            }
+            printf("*");
+            for (int j = 0; j < (i - 1) * 2 + 1; j++) {
+                printf(" ");
+            }
+            printf("*");
+        }
+        printf("\n");
+    }
+}
