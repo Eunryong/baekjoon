730acb3af55dd16a0bce32e78063a2dd4ff6cc80
Eunryong <86572427+Eunryong@users.noreply.github.com>
2024-06-20 20:20:21 +0900

    [Gold V] Title: 두 용액, Time: 68 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <limits.h>
+#include <algorithm>
+using namespace std;
+int arr[100001];
+int main() {
+    int n;
+    int ans = INT_MAX, idx1 = -1, idx2 = -1;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + n);
+    for (int i = 0; i < n - 1; i++) {
+        int st = i + 1;
+        int en = n - 1;
+        while (st < en) {
+            int mid = (st + en) / 2;
+            if (abs(arr[i] + arr[mid]) < ans) {
+                ans = abs(arr[i] + arr[mid]);
+                idx1 = i;
+                idx2 = mid;
+            }
+            if (arr[i] + arr[mid] < 0) {
+                st = mid + 1;
+            } else {
+                en = mid;
+            }
+        }
+        if (abs(arr[st] + arr[i]) < ans) {
+            ans = abs(arr[st] + arr[i]);
+            idx1 = i;
+            idx2 = st;
+        }
+    }
+    cout << arr[idx1] << " " << arr[idx2];
+}
